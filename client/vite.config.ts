cfa96204f022d27ec9d43a519fda26d0b31ab4e2
Rashmika Lakshan <wrlakshan@gmail.com>
2025-05-16 10:51:45 +0530

    Update Docker configurations and refine client service setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+ plugins: [react()],
+ server: {
+  watch: {
+   usePolling: true,
+  },
+  host: true,
+  strictPort: true,
+  port: 8080, 
+}
 })
